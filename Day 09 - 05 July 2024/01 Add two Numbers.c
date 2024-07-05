26e6f07adc03216e337c698003ae88d2615134ac
prasanth-vaindam <107233850+prasanth-vaindam@users.noreply.github.com>
2024-07-05 19:45:59 +0530

    scanf demo

---
+++ Day 09 - 05 July 2024/01 Add two Numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    // int a,b,sum;
+    // printf("Enter the values of a an")
+    
+    char grade;
+    printf("Enter your grade: ");
+    scanf("%c", &grade);
+
+    printf("My Grade is %c", grade);
+    return 0;
+}
